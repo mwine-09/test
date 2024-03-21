6583e4f794188b813507b3f1fac052accaf625de
Mwine <amumpairetumuramye09@gmail.com>
2024-03-21 07:57:11 +0300

    What is your name boy

--- deletes.py
+++ deletes.py
@@ -2,4 +2,7 @@ This is the file that we are going to delete as a the first transaction.
 
 This change is made in the branch 
 2
+
+what is your name boy?
+
 what is the name of the girl that you took out the other night?
