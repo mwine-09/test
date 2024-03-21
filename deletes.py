18c48bb34d63fba3356d385c955d72399471c85f
Mwine <amumpairetumuramye09@gmail.com>
2024-03-21 07:52:41 +0300

    altered something

--- deletes.py
+++ deletes.py
@@ -1,3 +1,5 @@
 This is the file that we are going to delete as a the first transaction.
 
-This change is made in the branch 2
+This change is made in the branch 
+2
+what is the name of the girl that you took out the other night?
